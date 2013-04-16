d4ee35a80643f927f20b57c6ba18c928ab0628ce
Joseph Deray <joseph.deray@wolfe.com>
2013-04-16 17:40:54 -0400

    Revert "[MPO-1161] - fixes for symfony 2.2"
    This reverts commit d07a948ada71e1f2101da42e648cc0bd8d45c4ba.

--- Listener/AesEncryption.php
+++ Listener/AesEncryption.php
@@ -3,19 +3,30 @@
 namespace Omni\EncryptionBundle\Listener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Local\MerchantBundle\Entity\Creditcard;
 
 class AesEncryption
 {
 	protected $encryptionManager;
+	protected $encrypted;
+	protected $decrypted;
 	
 	public function __construct($encryptionManager){
 		$this->encryptionManager = $encryptionManager;
+		$this->encrypted = new \SplObjectStorage();
+		$this->decrypted = new \SplObjectStorage();
 	}
 	
     public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
+        
+        if($this->encrypted->contains($entity)){
+        	
+        	return;
+        }
+        
         $entityManager = $args->getEntityManager();
 
         $factory = $entityManager->getMetadataFactory();
@@ -26,13 +37,20 @@ class AesEncryption
 				$setmethod = 'set'.ucfirst($key);
 				$getmethod = 'get'.ucfirst($key);
 				$entity->$setmethod($this->encryptionManager->aesEncrypt($entity->$getmethod()));
+				$this->encrypted->attach($entity);
 			}
 		}
     }
 	
-	public function preUpdate(LifecycleEventArgs $args)
+	public function preUpdate(PreUpdateEventArgs $args)
     {
         $entity = $args->getEntity();
+        
+        if($this->encrypted->contains($entity)){
+        	
+        	return;
+        }
+        
         $entityManager = $args->getEntityManager();
 
         $factory = $entityManager->getMetadataFactory();
@@ -42,7 +60,14 @@ class AesEncryption
 			if ($attributes['type'] == 'aescrypt'){
 				$setmethod = 'set'.ucfirst($key);
 				$getmethod = 'get'.ucfirst($key);
-				$entity->$setmethod($this->encryptionManager->aesEncrypt($entity->$getmethod()));
+				$newValue = $this->encryptionManager->aesEncrypt($entity->$getmethod());
+				$entity->$setmethod($newValue);
+				
+				if ($args->hasChangedField($key)) {
+					
+					$args->setNewValue($key, $newValue);
+				}
+				$this->encrypted->attach($entity);
 			}
 		}
     }
@@ -50,6 +75,12 @@ class AesEncryption
 	public function postLoad(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
+        
+        if ($this->decrypted->contains($entity)) {
+        	
+        	return;
+        }
+        
         $entityManager = $args->getEntityManager();
 		
 		$factory = $entityManager->getMetadataFactory();
@@ -60,9 +91,45 @@ class AesEncryption
 				$setmethod = 'set'.ucfirst($key);
 				$getmethod = 'get'.ucfirst($key);
 				$entity->$setmethod($this->encryptionManager->aesDecrypt($entity->$getmethod()));
+				$this->decrypted->attach($entity);
 			}
 		}
 		
     }
 	
+    public function postUpdate(LifecycleEventArgs $args) {
+    	
+    	return $this->postSave($args);
+    }
+    
+    public function postPersist(LifecycleEventArgs $args) {
+    	
+    	return $this->postSave($args);
+    }
+    
+    public function postSave(LifecycleEventArgs $args) {
+    	
+    	$entity = $args->getEntity();
+    	
+    	if (!$this->encrypted->contains($entity)) {
+    		
+    		return;
+    	}
+    	
+    	$entityManager = $args->getEntityManager();
+    	
+    	$factory = $entityManager->getMetadataFactory();
+    	$metadata = $factory->getMetadataFor(get_class($entity));
+    	
+    	foreach ($metadata->fieldMappings as $key => $attributes){
+    		if ($attributes['type'] == 'aescrypt'){
+    			$setmethod = 'set'.ucfirst($key);
+    			$getmethod = 'get'.ucfirst($key);
+    			$entity->$setmethod($this->encryptionManager->aesDecrypt($entity->$getmethod()));
+    			$this->decrypted->attach($entity);
+    		}
+    	}
+    	
+    	$this->encrypted->detach($entity);
+    }
 }
